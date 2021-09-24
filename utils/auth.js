67332badfbf2081896b9752187ca98026338fb24
lyndseyjw <lyndseyjwatson@gmail.com>
2021-09-24 10:03:00 -0700

    created all initial folders & files

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+// we inbed this in our route to render all blog posts & dashboard to verify the user is logged in
+
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
